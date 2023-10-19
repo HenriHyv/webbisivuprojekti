fbe48b42a1bb497ef5ea6a5f668aa7e297f0ee6c
Henri Hyvärinen <Henri.hyvarinen1@gmail.com>
2023-10-19 13:12:41 +0300

    vaikka ja mitä

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,4 @@
-// JavaScript for tab functionality (keep this separate)
+//scripti koodit
 var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
 
@@ -17,19 +17,33 @@ document.addEventListener("DOMContentLoaded", function() {
     var factTextElement = document.getElementById('factText');
     var radioButtons = document.querySelectorAll('input[name="fav_language"]');
 
-    // Add a change event listener to each radio button
+    if (!factTextElement) {
+        console.error('factTextElement not found. Check your HTML structure.');
+        return; 
+    }
+
+    // eventin kuuntelia jokaiselle napille
     radioButtons.forEach(function(radioButton) {
         radioButton.addEventListener('change', function() {
             // Check if the radio button is checked
             if (this.checked) {
                 var selectedOption = this.value;
-                // Update the paragraph text with the corresponding facts
-                if (selectedOption === 'Libra') {
-                    factTextElement.textContent = 'Librans are extroverted, cosy, and friendly people. Librans, like the Scales that symbolise the sign, are often concerned with attaining balance, harmony, peace, and justice in the world. With their vast stores of charm, intelligence, frankness, persuasion, and seamless connectivity, they are well-equipped to do so.';
-                } else if (selectedOption === 'CSS') {
-                    factTextElement.textContent = 'CSS is a styling language used for designing the layout and appearance of web pages.';
-                } else if (selectedOption === 'JavaScript') {
-                    factTextElement.textContent = 'JavaScript is a scripting language used for adding interactivity to web pages.';
+                // päivittää halutun tiedon klikatessa
+                try {
+                    if (selectedOption === 'Libra') {
+                        factTextElement.textContent = 'Librans are extroverted, cosy, and friendly people. Librans, like the Scales that symbolise the sign, are often concerned with attaining balance, harmony, peace, and justice in the world. With their vast stores of charm, intelligence, frankness, persuasion, and seamless connectivity, they are well-equipped to do so.';
+                    } else if (selectedOption === 'CSS') {
+                        factTextElement.textContent = 'CSS is a styling language used for designing the layout and appearance of web pages.';
+                    } else if (selectedOption === 'JavaScript') {
+                        factTextElement.textContent = 'JavaScript is a scripting language used for adding interactivity to web pages.';
+                    } else {
+                        throw new Error('Invalid option selected: ' + selectedOption);
+                    }
+                } catch (error) {
+                    
+                    console.error('ERROR!:', error.message);// näyttää errorin 
+                    
+                    alert('ERROR CAME, PLS TRY AGAIN.'); //Error mikä näytetään käyttäjälle alert boxina
                 }
             }
         });
