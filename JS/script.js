4b455dbdce2df81cacf4f0a2d8474644af5d2c00
Henri Hyvärinen <Henri.hyvarinen1@gmail.com>
2023-10-20 13:38:20 +0300

    ulkonäkö muutoksia ja github repon fetchaus

--- JS/script.js
+++ JS/script.js
@@ -19,7 +19,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     if (!factTextElement) {
         console.error('factTextElement not found. Check your HTML structure.');
-        return; 
+        return;
     }
 
     // eventin kuuntelia jokaiselle napille
@@ -32,20 +32,79 @@ document.addEventListener("DOMContentLoaded", function() {
                 try {
                     if (selectedOption === 'Libra') {
                         factTextElement.textContent = 'Librans are extroverted, cosy, and friendly people. Librans, like the Scales that symbolise the sign, are often concerned with attaining balance, harmony, peace, and justice in the world. With their vast stores of charm, intelligence, frankness, persuasion, and seamless connectivity, they are well-equipped to do so.';
-                    } else if (selectedOption === 'CSS') {
-                        factTextElement.textContent = 'CSS is a styling language used for designing the layout and appearance of web pages.';
-                    } else if (selectedOption === 'JavaScript') {
-                        factTextElement.textContent = 'JavaScript is a scripting language used for adding interactivity to web pages.';
+                    } else if (selectedOption === 'Family') {
+                        factTextElement.textContent = 'I have 5 siblings, 4 big sisters and 1 big brother. My brother is also in IT industry and my cousin is working at Remedy! it is a finnish gaming company';
+                    } else if (selectedOption === 'Hobbies') {
+                        factTextElement.textContent = 'Besides coding, i also enjoy jogging, gymnastics, video games and reading.';
                     } else {
                         throw new Error('Invalid option selected: ' + selectedOption);
                     }
                 } catch (error) {
-                    
-                    console.error('ERROR!:', error.message);// näyttää errorin 
-                    
+                    console.error('ERROR!:', error.message); // näyttää errorin
                     alert('ERROR CAME, PLS TRY AGAIN.'); //Error mikä näytetään käyttäjälle alert boxina
                 }
             }
         });
     });
+
+    var repoListElement = document.getElementById('repo-list');
+
+    // Function to fetch and display repositories
+    function fetchRepositories() {
+        // Fetch your GitHub repositories
+        fetch('https://api.github.com/users/henrihyv/repos')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                // Process and display repository data
+                var repositories = data.map(repo => ({
+                    name: repo.name,
+                    url: repo.html_url
+                }));
+
+                // Clear any existing content in the repo-list element
+                repoListElement.innerHTML = '';
+
+                // Display repositories on the page
+                repositories.forEach(repo => {
+                    var repoElement = document.createElement('div');
+                    repoElement.innerHTML = `<a href="${repo.url}" target="_blank">${repo.name}</a>`;
+                    repoListElement.appendChild(repoElement);
+                });
+            })
+            .catch(error => {
+                // Handle errors here
+                console.error('Fetch error:', error);
+            });
+    }
+
+    // Call the fetchRepositories function to load repositories
+    fetchRepositories();
+    
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    // HTML elements
+    var toggleButton = document.getElementById('toggle-button');
+    var repoList = document.getElementById('repo-list');
+
+    // Function to toggle the visibility of the repository list
+    function toggleRepoList() {
+        if (repoList.style.display === 'none' || repoList.style.display === '') {
+            repoList.style.display = 'block';
+            toggleButton.textContent = 'Hide Repositories';
+        } else {
+            repoList.style.display = 'none';
+            toggleButton.textContent = 'Show Repositories';
+        }
+    }
+
+    // Add a click event listener to the toggle button
+    toggleButton.addEventListener('click', toggleRepoList);
+    
+});
+
